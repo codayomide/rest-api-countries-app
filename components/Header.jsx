2383112decb17ff4ca98152d8d9a101dd44d97b8
codayomide <smokayomide@gmail.com>
2023-08-02 21:07:57 +0100

    Adjusted style for all screens in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between px-4 py-7">
-      <h2 className="font-extrabold">Where in the world?</h2>
-      <div>Dark Mode</div>
+    <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between items-center px-4 xs:px-6 xl:px-[65px] sm:px-12 py-6 ss:py-7">
+      <h2 className="font-extrabold text-sm ss:text-lg">Where in the world?</h2>
+      <div className="text-xs ss:text-sm">Dark Mode</div>
     </header>
   );
 }
