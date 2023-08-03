f1e424caced3257043469c85bc519e0df3a61e33
codayomide <smokayomide@gmail.com>
2023-08-03 09:53:40 +0100

    added icons to dark mode toggle button

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,24 @@
+'use client'
+
+import { useState } from 'react';
+import { FaSun } from 'react-icons/fa';
+import { BiSolidMoon } from 'react-icons/bi'
+
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between items-center px-4 xs:px-6 xl:px-[65px] sm:px-12 py-6 ss:py-7">
       <h2 className="font-extrabold text-sm ss:text-lg">Where in the world?</h2>
-      <div className="text-xs ss:text-sm">Dark Mode</div>
+      <button className="text-xs ss:text-sm flex items-center">
+        {darkMode ? <>
+            <FaSun className="header_icons" /> <span>Light Mode</span>
+          </> : 
+          <>
+            <BiSolidMoon className="header_icons" /> <span>Dark Mode</span>
+          </>
+        }
+      </button>
     </header>
   );
 }
