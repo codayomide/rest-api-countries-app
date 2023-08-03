ebc36f3e82f23b6a128250468b39b0bd40891c3a
codayomide <smokayomide@gmail.com>
2023-08-03 11:01:37 +0100

    minor change

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between items-center px-4 xs:px-6 xl:px-[65px] sm:px-12 py-6 ss:py-7">
+    <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between items-center px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6 ss:py-7">
       <h2 className="font-extrabold text-sm ss:text-lg">Where in the world?</h2>
       <button className="text-xs ss:text-sm flex items-center">
         {darkMode ? <>
