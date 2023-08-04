097496da5a114b66e5209c430b6a8dd0b701ef07
codayomide <smokayomide@gmail.com>
2023-08-04 02:11:53 +0100

    Made dark mode button text bolder

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between items-center px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6 ss:py-7">
       <h2 className="font-extrabold text-sm ss:text-lg md:text-2xl">Where in the world?</h2>
-      <button className="text-xs ss:text-sm md:text-lg flex items-center">
+      <button className="text-xs font-semibold ss:text-sm md:text-lg flex items-center">
         {darkMode ? <>
             <FaSun className="header_icons" /> <span>Light Mode</span>
           </> : 
