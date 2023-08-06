b69f4234f688db9d06428b320584325c25e1732e
codayomide <smokayomide@gmail.com>
2023-08-06 15:13:31 +0100

    implemented context API to pass data

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,12 @@ import Countries from "@components/Countries";
 import SearchAndFilter from "@components/SearchAndFilter";
 import { useState, useEffect, createContext } from "react";
 
-
+export const CountriesContext = createContext();
 
 const Home = () => {
   const [countries, setCountries] = useState([]);
   const [isPending, setIsPending] = useState(true);
+  const [isFiltered, setIsFiltered] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,19 +22,20 @@ const Home = () => {
           setIsPending(false);
         })
       } catch (err) {
-        console.log;
+        console.log(err);
       }
     };
-    fetchData()
+    fetchData();
   }, []);
 
   return ( 
-    <div className="Home bg-lmBgLightGray w-full h-fit px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6">
-      <SearchAndFilter />
-
-      { isPending && <div>Loading...</div> }
-      { countries && <Countries countries={countries} /> }      
-    </div>
+    <CountriesContext.Provider value={{ isFiltered, setIsFiltered, countries }}>
+      <div className="Home bg-lmBgLightGray w-full h-fit px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6">
+        <SearchAndFilter data={countries} />
+        { isPending && <div>Loading...</div> }
+        { countries && <Countries countries={countries} /> }
+      </div>
+    </CountriesContext.Provider>
   );
 }
  
--- components/Search.jsx
+++ components/Search.jsx
@@ -1,10 +1,27 @@
+import { useContext, useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai/';
+import { CountriesContext } from '@app/page';
 
 const Search = () => {
+  const [searchInput, setsearchInput] = useState('');
+
+  const { isFiltered, setIsFiltered, countries } = useContext(CountriesContext)
+
+  const handleSearch = () => {
+    console.log(searchInput);
+  }
+
   return (
     <div className="searchContainer relative w-full md:w-[350px] mb-10 md:mb-0">
         <AiOutlineSearch className="text-lmDarkGray text-[23px] absolute top-[50%] translate-y-[-50%] left-6 " />
-        <input type="search" placeholder="Search for a country..." className="block appearance-none w-full bg-white shadow-md text-gray-700 text-[13px] py-4 px-4 pl-[4rem] rounded leading-tight outline-none object-cover" />
+
+        <input
+          type="search" 
+          placeholder="Search for a country..." 
+          className="block appearance-none w-full bg-white shadow-md text-gray-700 text-[13px] py-4 px-4 pl-[4rem] rounded leading-tight outline-none object-cover"
+          value={searchInput}
+          onChange={(e) => setsearchInput(e.target.value)}
+        />
       </div>
   );
 }
