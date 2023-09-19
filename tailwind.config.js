178f05b9dc2dde77f835a7da4c9e723956656394
codayomide <smokayomide@gmail.com>
2023-09-19 14:37:54 +0100

    Added darkMode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
