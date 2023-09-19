460b32732db6553de353513838e124d13f51c163
codayomide <smokayomide@gmail.com>
2023-09-19 13:24:01 +0100

    Check border countries

--- components/BorderCountries.jsx
+++ components/BorderCountries.jsx
@@ -8,43 +8,52 @@ const BorderCountries = ({ individualCountry }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await fetch('https://restcountries.com/v2/all')
-
-        .then((res) => res.json())
-        .then((data) => {
-          setAllCountries(data);
-        })
+        await fetch("https://restcountries.com/v2/all")
+          .then((res) => res.json())
+          .then((data) => {
+            setAllCountries(data);
+          });
       } catch (err) {
-        console.log;
+        console.log(err);
       }
     };
-    fetchData()
+    fetchData();
   }, []);
 
   const getFullName = (country, data) => {
-    data.map(targetData => {
+    data.map((targetData) => {
       if (country === targetData.alpha3Code) {
-        country = targetData.name
+        country = targetData.name;
       }
-    })
+    });
 
     return country;
-  }
+  };
 
   return (
-    <div className="md:flex md:flex-row md:items-center">
-      <h1 className="font-semibold mb-2 md:mr-4">Border Countries:</h1>
-      <div className="flex flex-row flex-wrap">
-        {individualCountry.borders && individualCountry.borders.map((borderCountry, index) => (
-          <Link 
-            href={`/countries/${getFullName(borderCountry, allCountries)}`} 
-            key={index} 
-            className="text-lmVeryDarkBlue bg-neutralWhite shadow-lg mr-2 mb-4 px-4 py-1"
-          >{getFullName(borderCountry, allCountries)}</Link>
-        ))}
-      </div>
-    </div>
+    <>
+      {individualCountry.borders && (
+        <div className="md:flex md:flex-row md:items-center">
+          <h1 className="font-semibold mb-2 md:mr-4">Border Countries:</h1>
+          <div className="flex flex-row flex-wrap">
+            {individualCountry.borders &&
+              individualCountry.borders.map((borderCountry, index) => (
+                <Link
+                  href={`/countries/${getFullName(
+                    borderCountry,
+                    allCountries
+                  )}`}
+                  key={index}
+                  className="text-lmVeryDarkBlue bg-neutralWhite shadow-lg mr-2 mb-4 px-4 py-1"
+                >
+                  {getFullName(borderCountry, allCountries)}
+                </Link>
+              ))}
+          </div>
+        </div>
+      )}
+    </>
   );
-}
- 
+};
+
 export default BorderCountries;
