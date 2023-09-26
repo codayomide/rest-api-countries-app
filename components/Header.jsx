62882dde3afd0a6ccefba59859250acdae7d5cf1
codayomide <smokayomide@gmail.com>
2023-09-26 20:09:20 +0100

    Worked on header darkmode

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <header className="bg-lmBgLightGray text-lmVeryDarkBlue drop-shadow w-full flex justify-between items-center px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6 ss:py-7">
+    <header className="bg-lmBgLightGray dark:bg-dmBlue text-lmVeryDarkBlue dark:text-neutralWhite drop-shadow w-full flex justify-between items-center px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6 ss:py-7">
       <h2 className="font-extrabold text-sm ss:text-lg md:text-2xl">
         Where in the world?
       </h2>
@@ -17,13 +17,13 @@ const Header = () => {
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
         {theme === "light" ? (
-          <>
+          <div>
             <FaSun className="header_icons" /> <span>Light Mode</span>
-          </>
+          </div>
         ) : (
-          <>
+          <div>
             <BiSolidMoon className="header_icons" /> <span>Dark Mode</span>
-          </>
+          </div>
         )}
       </button>
     </header>
