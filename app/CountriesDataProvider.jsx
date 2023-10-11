b02a3fdb4943c5e471fa7f3a5b22f995c25c4e6c
codayomide <smokayomide@gmail.com>
2023-10-11 11:22:44 +0100

    minor fix

--- app/CountriesDataProvider.jsx
+++ app/CountriesDataProvider.jsx
@@ -5,41 +5,46 @@ import { createContext } from "react";
 
 export const CountriesContext = createContext();
 
-export async function getServerSideProps() { 
-  let countriesData = 'hi';
+export async function getServerSideProps() {
+  let countriesData = null;
   let isLoading = true;
 
   try {
-    const res = await fetch('https://restcountries.com/v2/all');
+    const res = await fetch("https://restcountries.com/v2/all");
     countriesData = await res.json();
+    console.log(countriesData)
     isLoading = false;
-
-    return {
-      props: {
-        countriesData,
-        isLoading
-      },
-    };
 
   } catch (err) {
     console.log(err);
     isLoading = false;
-
-    return {props: {}}
   }
+
+  return {
+    props: {
+      countriesData,
+      isLoading,
+    },
+  };
 }
 
 const CountriesDataProvider = ({ countriesData, isLoading, children }) => {
-  const [countries, setCountries] = useState(countriesData)
+  const [countries, setCountries] = useState(countriesData);
   const [filteredData, setFilteredData] = useState(countries);
 
-  console.log(countries)
+  console.log(countriesData);
 
   useEffect(() => {
-    setCountries(countriesData)
-  }, [countriesData])
+    setCountries(countriesData);
+  }, [countriesData]);
 
-  return <CountriesContext.Provider value={{ countries, isLoading, filteredData, setFilteredData }}>{children}</CountriesContext.Provider>
-}
- 
+  return (
+    <CountriesContext.Provider
+      value={{ countries, isLoading, filteredData, setFilteredData }}
+    >
+      {children}
+    </CountriesContext.Provider>
+  );
+};
+
 export default CountriesDataProvider;
