0e63c9c3644141425e814444b82cf52993e407f8
Ayomide Adegoke <smokayomide@gmail.com>
2025-04-02 11:04:19 +0100

    Working on SSG

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import Countries from "@components/Countries";
@@ -8,38 +8,54 @@ import Loading from "@components/Loading";
 
 export const CountriesContext = createContext();
 
-const Home = () => {
-  const [countries, setCountries] = useState([]);
-  const [isPending, setIsPending] = useState(true);
+export async function getStaticProps() {
+  const res = await fetch("https://restcountries.com/v2/all");
+  const countries = await res.json();
+
+  return { props: { countries } };
+}
+
+const Home = ({ countries }) => {
+  // const [countries, setCountries] = useState([]);
+  // const [isPending, setIsPending] = useState(true);
   const [isFiltered, setIsFiltered] = useState(false);
   const [filteredData, setFilteredData] = useState(countries);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await fetch('https://restcountries.com/v2/all')
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       await fetch("https://restcountries.com/v2/all")
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           setCountries(data);
+  //           // setIsPending(false);
+  //         });
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
-        .then((res) => res.json())
-        .then((data) => {
-          setCountries(data);
-          setIsPending(false);
-        })
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
+  console.log(countries)
 
-  return ( 
-    <CountriesContext.Provider value={{ isFiltered, setIsFiltered, countries, filteredData, setFilteredData }}>
+  return (
+    <CountriesContext.Provider
+      value={{
+        isFiltered,
+        setIsFiltered,
+        countries,
+        filteredData,
+        setFilteredData,
+      }}
+    >
       <div className="Home bg-lmBgLightGray dark:bg-dmBgVeryDarkBlue w-full h-full flex-grow flex flex-col px-4 xs:px-6 sm:px-12 xl:px-[65px] py-6 relative">
-        <SearchAndFilter data={countries} />
-        { isPending && <Loading /> }
-        { filteredData && <Countries countries={filteredData} /> }
+        {/* <SearchAndFilter data={countries} /> */}
+
+        {filteredData && <Countries countries={filteredData} />}
       </div>
     </CountriesContext.Provider>
   );
-}
- 
+};
+
 export default Home;
